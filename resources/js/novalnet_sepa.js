e840886a4ad58ef554de35b0d8065d4ca9a1a462
kotteeswaran <kotteeswaranviit@gmail.com>
2019-03-11 17:38:12 +0530

    Update novalnet_sepa.js

--- resources/js/novalnet_sepa.js
+++ resources/js/novalnet_sepa.js
@@ -1,5 +1,6 @@
 jQuery(document).ready( function() {
-	jQuery('#nn_sepa_iban').on('keypress',function ( event ) {
-		$(this).val().replace( /[^a-zA-Z 0-9]+/g, "" ).replace( /\s+/g, "" );
+	jQuery('#nn_sepa_iban').on('input',function ( event ) {
+		let iban = $(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
+    		$(this).val(iban);   
 	});
 });
