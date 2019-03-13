09762a719cf5c2968a2d92039018e3b125f61d70
kotteeswaran <kotteeswaranviit@gmail.com>
2019-03-13 13:03:38 +0530

    Update CaptureEventProcedure.php

--- src/Procedures/CaptureEventProcedure.php
+++ src/Procedures/CaptureEventProcedure.php
@@ -90,7 +90,7 @@ class CaptureEventProcedure
 	    
         $this->getLogger(__METHOD__)->error('EventProcedure.triggerFunction', ['order' => $order]);
 	    if(in_array($status, ['85', '91', '98', '99'])) {
-        $this->paymentHelper->doCaptureVoid($order, $paymentDetails, $tid, $key, true, $bank_name);
+        $this->paymentHelper->doCaptureVoid($order, $paymentDetails, $tid, $key, $bank_name, true);
 	    } 
 
     }
